fb11f42c4e30e7bec32cac311c098da27195070e
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-19 03:23:04 -0700

    Created get lambda to fetch yelp businesses

--- restaurants.js
+++ restaurants.js
@@ -2,30 +2,14 @@ import handler from "./libs/handler-lib";
 import axios from "axios";
 
 export const main = handler(async(event, context) => {
-  //request is going to have location points and search
-  const data = JSON.parse(event.body);
-  const params = {
-    latitude: data.latitude,
-    longitude: data.longitude,
-    term: data.term
-  };
-
+  const params = event.queryStringParameters;
   const url = `https://api.yelp.com/v3/businesses/search?term=${params.term}&latitude=${params.latitude}&longitude=${params.longitude}`;
 
-  async function retrieveRestaurants() {
-    let data;
+  const results = await axios.get(url, {
+    headers: {
+      "Authorization": `Bearer ${process.env.YELP_KEY}`
+    }
+  });
 
-    axios.get(url, {
-      headers: {
-        "Authorization": `Bearer ${process.env.yelpKey}`
-      }
-    })
-    .then((results) => {
-      data = results;
-    });
-
-    return data;
-  }
-
-  return retrieveRestaurants();
+  return results.data;
 });
